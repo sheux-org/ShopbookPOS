38b5f2a3121ce40fca8c89cff1a938872288fcaa
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2026-05-27 13:25:05 +0530

    refactor: move outputFileTracingRoot out of experimental config block in next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  outputFileTracingRoot: path.join(__dirname, '../'),
 };
 
 module.exports = nextConfig;
